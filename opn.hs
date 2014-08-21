64bb82544c43e6409aae970681788c2a1076f84a
Anders Claesson <akc@akc.is>
2014-08-21 12:21:50 +0100

    Transpose the 'associations' section of .opnrc

--- opn.hs
+++ opn.hs
@@ -9,8 +9,9 @@ import           Data.Maybe (catMaybes)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as M
-import           Data.Ini (Ini(..), parseIni, lookupValue)
+import           Data.Ini --(Ini(..), parseIni)
 import           Control.Monad (forM_)
 import           System.Process (runProcess, readProcess)
 import           System.Environment (getArgs)
@@ -23,15 +24,10 @@ type Command   = Text
 type PathOrURL = String
 type MimeType  = Text
 type Extension = Text
+type Assoc     = HashMap Extension Command
 
-lookupBrowser :: Ini -> Command
-lookupBrowser = either error id . lookupValue "browser" "browser"
-
-lookupCommand :: Ini -> Extension -> Maybe Command
-lookupCommand (Ini ini) ext =
-    case M.lookup "associations" ini of
-        Nothing   -> error "Couldn't find required section 'associations'"
-        Just cmds -> T.strip `fmap` M.lookup (T.drop 1 ext) cmds
+lookupCommand :: Assoc -> Extension -> Maybe Command
+lookupCommand assoc ext = T.strip `fmap` M.lookup (T.drop 1 ext) assoc
 
 getMimeType :: FilePath -> IO MimeType
 getMimeType fpath = fmap
@@ -39,19 +35,17 @@ getMimeType fpath = fmap
     (readProcess "file" ["--mime-type", "-L", fpath] "")
 
 getExtensions :: FilePath -> IO [Extension]
-getExtensions fpath =
-    case takeExtension fpath of
-        []  -> guessExtensions `fmap` getMimeType fpath
-        ext -> return [T.pack ext]
+getExtensions fpath = do
+    es <- guessExtensions `fmap` getMimeType fpath
+    return $ case takeExtension fpath of { [] -> es; e -> T.pack e : es }
 
-getCommand :: Ini -> PathOrURL -> IO Command
-getCommand conf s = do
-    let browser = lookupBrowser conf
+getCommand :: (Text, Assoc) -> PathOrURL -> IO Command
+getCommand (browser, assoc) s = do
     fileExists <- doesFileExist s
     if fileExists
         then do
             exts <- getExtensions s
-            case catMaybes (map (lookupCommand conf) exts) of
+            case catMaybes (map (lookupCommand assoc) exts) of
                 []      -> return browser
                 (cmd:_) -> return cmd
         else
@@ -70,6 +64,16 @@ readConfig home = do
     opnrc = home </> ".opnrc"
     adjust = T.unlines . map T.stripStart . T.lines
 
+trConfig :: Ini -> (Command, Assoc)
+trConfig (Ini ini) = (browser, d)
+  where
+    d = M.fromList $ M.toList cmdDict >>= \(c,es) -> [(e,c) | e <- T.words es]
+    browser  = let s = "browser" in maybe (errKey s) id $ M.lookup s (fndSec s)
+    cmdDict  = fndSec "associations"
+    fndSec s = maybe (errSec s) id $ M.lookup s ini
+    errSec s = error $ "Couldn't find required section " ++ show s
+    errKey s = error $ "Couldn't find required key "     ++ show s
+
 run :: Command -> [String] -> IO ()
 run cmd args =
     runProcess (T.unpack cmd) args Nothing Nothing Nothing Nothing Nothing >>
@@ -81,6 +85,6 @@ main = do
     if null args
         then putStrLn "usage: opn (file | url)..."
         else do
-            conf <- readConfig =<< getHomeDirectory
+            conf <- trConfig `fmap` (readConfig =<< getHomeDirectory)
             forM_ args $ \s ->
                 getCommand conf s >>= \cmd -> run cmd [s]
